2a86f5b169cd35e13aec8e0d2f6c9dd466b0f3de
PeteGameDev <P.Richardson@wakefield.ac.uk>
2022-02-21 09:09:21 +0000

    rotate

--- Assets/DO NOT TOUCH/Scripts/FPS_PickupRotate.cs
+++ Assets/DO NOT TOUCH/Scripts/FPS_PickupRotate.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class FPS_PickupRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, +0.5f, 0f, Space.Self);
     }
 }
